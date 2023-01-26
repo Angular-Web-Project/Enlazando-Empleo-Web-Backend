e0e4e0d630793e0714b0b318832670212c06333d
Salinas Cruz Edison <edison.salinasc@ug.edu.ec>
2023-01-26 15:39:21 -0500

    Estructura de Spring Boot

---
+++ src/test/java/com/backend/enlazandoempleoweb/EnlazandoEmpleoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.enlazandoempleoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnlazandoEmpleoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
