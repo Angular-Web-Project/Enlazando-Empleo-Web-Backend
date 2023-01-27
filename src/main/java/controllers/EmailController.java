c071339163fb3040e1d7375bbb97454e0fa02220
Edison <edison.salinasc@ug.edu.ec>
2023-01-27 20:57:11 +0000

    'controller2'

--- src/main/java/controllers/EmailController.java
+++ src/main/java/controllers/EmailController.java
@@ -1,5 +1,23 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import services.EmailService;
+
+@RestController
 public class EmailController {
+	
+	@Autowired
+	EmailService emailService;
+	
+	@GetMapping("/email/send")
+	public ResponseEntity<?> sendEmail() {
+		emailService.sendEmail();
+		return new ResponseEntity("envio exitoso", HttpStatus.OK);
+	}
 
 }
