55490bc6e2ea5dc7aab8da5c1ab81a66a4aea274
Salinas Edison <edison.salinasc@ug.edu.ec>
2023-02-02 20:20:52 +0000

    Capa Entidad

---
+++ src/main/java/com/backend/enlazandoempleoweb/entidades/Usuario.java
@@ -0,0 +1,26 @@
+package com.backend.enlazandoempleoweb.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private boolean enabled =true;
+	private String perfil;
+	
+	
+}
